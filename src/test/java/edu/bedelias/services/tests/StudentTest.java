535d3010d5491ea3ccc035ec1c28ee2c748af4cc
Gas <cilindrox@gmail.com>
2012-11-01 21:32:39 -0200

    AppContext setup for Hibernate.
    Signed-off-by: Gas <cilindrox@gmail.com>

--- src/test/java/edu/bedelias/services/tests/StudentTest.java
+++ src/test/java/edu/bedelias/services/tests/StudentTest.java
@@ -13,8 +13,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import edu.bedelias.entities.Student;
 import edu.bedelias.services.StudentService;
@@ -25,8 +23,6 @@ import edu.bedelias.services.StudentService;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:applicationContextTest.xml"})
-@TransactionConfiguration
-@Transactional
 public class StudentTest {
 
 	@Autowired
