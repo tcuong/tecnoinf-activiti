70734da129c169965401ac58793c0233c8f77aec
gfestari <cilindrox@gmail.com>
2012-11-08 20:07:03 -0300

    SuperPOM refactoring. FUCK THE POLICE
    Signed-off-by: gfestari <cilindrox@gmail.com>

---
+++ bedelias-activiti/src/test/java/edu/bedelias/services/tests/StudentTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package edu.bedelias.services.tests;
+
+//import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.bedelias.entities.Student;
+import edu.bedelias.services.StudentService;
+
+/**
+ * @author Gas
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContextTest.xml"})
+public class StudentTest {
+
+	@Autowired
+	private StudentService studentService;
+
+	@Test
+	public void test() {
+		Student student = new Student();
+		student.setName("Chupito");
+		student.setEmail("chupame@eltobonia.net");
+		student.setCedula("someCedula");
+		
+		student = studentService.createStudent(student);
+		
+		assertNotNull("Student can't be null", student);
+		assertEquals("Email must be the same", student.getEmail(),
+				"chupame@eltobonia.net");
+	}
+
+	// Getters && Setters
+
+	public StudentService getStudentService() {
+		return studentService;
+	}
+
+	public void setStudentService(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
+}
