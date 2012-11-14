08485b42cbce5fb99ee0ba3c025e02a5837a2533
gfestari <cilindrox@gmail.com>
2012-11-14 21:34:21 -0200

    PDF functionality.
    Signed-off-by: gfestari <cilindrox@gmail.com>

---
+++ bedelias-activiti/src/test/java/edu/bedelias/services/tests/ActaTest.java
@@ -0,0 +1,16 @@
+package edu.bedelias.services.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.bedelias.utils.ReportsService;
+
+public class ActaTest {
+
+	@Test
+	public void test() {
+		ReportsService.main(null);
+	}
+
+}
