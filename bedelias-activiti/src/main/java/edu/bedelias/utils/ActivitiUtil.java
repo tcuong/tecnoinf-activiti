d68057e5a4c0165a00816541c4814c3fb7976169
gfestari <cilindrox@gmail.com>
2012-11-22 23:38:43 -0200

    (Hopefully) fixed Spring autowiring issue in activitiEngine (DefaultViewManager.java);
    See: http://jira.codehaus.org/browse/ACT-1420 and http://activiti.org/javadocs/org/activiti/engine/test/ActivitiTestCase.html
    
    Signed-off-by: gfestari <cilindrox@gmail.com>

--- bedelias-activiti/src/main/java/edu/bedelias/utils/ActivitiUtil.java
+++ bedelias-activiti/src/main/java/edu/bedelias/utils/ActivitiUtil.java
@@ -6,16 +6,16 @@ import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RuntimeService;
 
 public class ActivitiUtil {
-    public static ProcessEngine getProcessEngine() {
-
-//        ProcessEngine pe = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration()
-//                .buildProcessEngine();
-        ProcessEngine pe = ProcessEngines.getDefaultProcessEngine();
-
-        return pe;
-    }
-
-    public static RuntimeService getRuntimeService() {
-        return getProcessEngine().getRuntimeService();
-    }
+//    public static ProcessEngine getProcessEngine() {
+//
+////        ProcessEngine pe = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration()
+////                .buildProcessEngine();
+//        ProcessEngine pe = ProcessEngines.getDefaultProcessEngine();
+//
+//        return pe;
+//    }
+//
+//    public static RuntimeService getRuntimeService() {
+//        return getProcessEngine().getRuntimeService();
+//    }
 }
