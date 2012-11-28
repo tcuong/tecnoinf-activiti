4c9fbf4d2eedf52e76d70559e25f57d2abe5348b
gfestari <cilindrox@gmail.com>
2012-11-28 19:47:37 -0200

    GetProcessEngine functionality. Web tests.
    Signed-off-by: gfestari <cilindrox@gmail.com>

---
+++ bedelias-web/src/test/java/edu/bedelias/tests/activiti/ActivitiEngineTest.java
@@ -0,0 +1,26 @@
+package edu.bedelias.tests.activiti;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.activiti.engine.ProcessEngine;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContextTest-c3p0.xml"} )
+public class ActivitiEngineTest {
+
+	@Test
+	public void CallActivitiEngine() {
+		ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext("classpath:activiti.cfg.xml");
+		ProcessEngine pe = (ProcessEngine)cpx.getBean("processEngine");
+		pe.getRuntimeService().startProcessInstanceById("generarActa");
+		
+		assertNotNull("ProcessEngine can't be null", pe);
+	}
+
+}
