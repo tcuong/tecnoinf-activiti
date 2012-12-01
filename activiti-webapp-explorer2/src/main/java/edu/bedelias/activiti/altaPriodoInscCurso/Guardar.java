3df8cac81fda6db12754fdec77d5937c1b65688f
Zulmo <mathiasnu@gmail.com>
2012-12-01 15:46:56 -0200

    Agregado al appContext la interface de PeriodoInsc
    Signed-off-by: Zulmo <mathiasnu@gmail.com>

--- activiti-webapp-explorer2/src/main/java/edu/bedelias/activiti/altaPriodoInscCurso/Guardar.java
+++ activiti-webapp-explorer2/src/main/java/edu/bedelias/activiti/altaPriodoInscCurso/Guardar.java
@@ -1,31 +1,39 @@
 package edu.bedelias.activiti.altaPriodoInscCurso;
 
+import java.util.Date;
+
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.apache.log4j.Logger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.bedelias.entities.PeriodoInscripcion;
+import edu.bedelias.services.PeriodoInscripcionService;
 
 public class Guardar implements JavaDelegate {
 
 	private Logger logger = Logger.getLogger(Guardar.class);
+	private PeriodoInscripcionService periodoInscripcionService;
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
-		logger.info("TAMOOOO ACAAAAAAAAA");
+
+		ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext(
+				"classpath:applicationContextRemote.xml");
+		periodoInscripcionService = (PeriodoInscripcionService) cpx
+				.getBean("periodoInscripcionService");
+
+		Date fechaInicio = (Date) execution.getVariable("fechaInicio");
+		Date fechaFinComienzoDesistir = (Date) execution
+				.getVariable("fechaFinComienzoDesistir");
+		Date fechaFin = (Date) execution.getVariable("fechaFin");
+		String descripcion = execution.getVariable("descripcion").toString();
+
+		PeriodoInscripcion periodoInscripcion = new PeriodoInscripcion();
+		periodoInscripcion.setFechaInicio(fechaInicio);
+		periodoInscripcion.setFechaDesist(fechaFinComienzoDesistir);
+		periodoInscripcion.setFechaFin(fechaFin);
+		// periodoInscripcion
 
 	}
 
--- activiti-webapp-explorer2/src/main/java/edu/bedelias/activiti/altaPriodoInscCurso/ValidarFecha.java
+++ activiti-webapp-explorer2/src/main/java/edu/bedelias/activiti/altaPriodoInscCurso/ValidarFecha.java
@@ -4,31 +4,16 @@ import java.util.Date;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
-import org.apache.log4j.Logger;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import edu.bedelias.entities.Carreer;
-import edu.bedelias.services.CarreerService;
 
 public class ValidarFecha implements JavaDelegate {
 
-	private Logger logger = Logger.getLogger(Guardar.class);
-
-	private CarreerService carreerService;
-
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext(
-				"classpath:applicationContextRemote.xml");
-		carreerService = (CarreerService) cpx.getBean("carreerService");
 
 		boolean valido = false;
-		logger.info("000000000000000000000000000000000");
-		logger.info("000000000000000000000000000000000");
-		logger.info("000000000000000000000000000000000");
+		String msg = "El orden de las fechas es incorrecto.";
 
 		Date fechaInicio = (Date) execution.getVariable("fechaInicio");
-		logger.info("FECHA " + fechaInicio);
 
 		Date fechaFinComienzoDesistir = (Date) execution
 				.getVariable("fechaFinComienzoDesistir");
@@ -37,20 +22,10 @@ public class ValidarFecha implements JavaDelegate {
 		if (fechaInicio.before(fechaFinComienzoDesistir)
 				&& fechaFinComienzoDesistir.before(fechaFin)) {
 			valido = true;
+			msg = "Se ha completado la tarea correctamente";
 		}
 		execution.setVariable("validar", valido);
-
-		Carreer carreer = carreerService.findCarreer(new Long(1));
-		logger.info("000000000000000000000000000000000 " + carreer.getName());
-
-	}
-
-	public CarreerService getCarreerService() {
-		return carreerService;
-	}
-
-	public void setCarreerService(CarreerService carreerService) {
-		this.carreerService = carreerService;
+		execution.setVariable("msg", msg);
 	}
 
 }
