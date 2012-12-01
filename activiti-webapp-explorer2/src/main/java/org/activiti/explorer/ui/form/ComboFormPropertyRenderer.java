0b9d434516884019d91547dc701319e5b9880d31
brus <brus@Brus-PC.(none)>
2012-12-01 11:57:21 -0200

    agregue propiedades en los form

---
+++ activiti-webapp-explorer2/src/main/java/org/activiti/explorer/ui/form/ComboFormPropertyRenderer.java
@@ -0,0 +1,47 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.explorer.ui.form;
+
+import org.activiti.engine.form.FormProperty;
+
+import com.vaadin.ui.AbstractSelect.Filtering;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Field;
+
+/**
+ * @author Frederik Heremans
+ */
+public class ComboFormPropertyRenderer extends AbstractFormPropertyRenderer {
+
+	public ComboFormPropertyRenderer() {
+		super(ComboFormType.class);
+	}
+	
+	private static final String[] cities = new String[] { "Berlin", "Brussels",
+        "Helsinki", "Madrid", "Oslo", "Paris", "Stockholm" };
+
+	@Override
+	public Field getPropertyField(FormProperty formProperty) {
+		
+		ComboBox combo = new ComboBox("Please select a city");
+        for (int i = 0; i < cities.length; i++) {
+            combo.addItem(cities[i]);
+        }
+
+        combo.setFilteringMode(Filtering.FILTERINGMODE_OFF);
+        combo.setImmediate(true);
+        
+		return combo;
+	}
+}
