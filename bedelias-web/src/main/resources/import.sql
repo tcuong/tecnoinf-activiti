1dc40f9c05bdd20a863209ea57501b52d0f54038
Brus <brunovierag@gmail.com>
2012-12-15 12:57:20 -0200

    Parte de la carga de datos y algunos enchules a la web

--- bedelias-web/src/main/resources/import.sql
+++ bedelias-web/src/main/resources/import.sql
@@ -1,26 +1,13 @@
-INSERT INTO `Student` VALUES (1,'2012-11-25 15:37:57',NULL,'47720720',NULL,'brunoviera@gmail.com',NULL,'Ramon Dominguez',NULL,'2012-11-25 15:37:57', null, null, 'password', '249e543a-99e8-48eb-8a0d-f9d3f5dab090'), (2,'2012-11-25 15:37:57',NULL,'47720734',NULL,'paploperez@gmail.com',NULL,'Pablo Perez',NULL,'2012-11-25 15:37:57', null, null, 'password', '249f543a-99e8-48eb-8a0d-f9d3f5dab090');
+INSERT INTO `Carreer` VALUES (1,'2012-11-25 00:00:00','Tecnólogo en Informática','tecno-inf',0,'N/A',40 ), (2,'2012-11-25 00:00:00','Ingenieria Eléctrica','ing-elec',15,'Electricista',20 ), (3,'2012-11-25 00:00:00','Ingenieria Química','ing-quim',15,'Laboratorista',30 );
 
-INSERT INTO `Carreer` VALUES (1,'2012-11-25 15:37:57','Ingenieria Mecanica','2431c36c-7533-4254-8f55-6f8a39934989',NULL,NULL,NULL),(2,'2012-11-25 15:37:57','Ingenieria Civil','1e11c85a-3215-473e-9e32-5e7f79532bd2',NULL,NULL,NULL),(3,'2012-11-25 15:37:57','Ingenieria Quimica','fc99c656-6527-403a-acef-370fe2e09f42',NULL,NULL,NULL),(4,'2012-11-25 15:37:57','Ingenieria Computacion','d45b0cc7-2c71-4095-a73f-e4a5db1b603b',NULL,NULL,NULL),(5,'2012-11-25 15:37:57','Ing. Alimentos','97cb7cda-702d-4b80-8901-80c8f7942c74',NULL,NULL,NULL);
+INSERT INTO `Materia` VALUES (1,'2012-11-25 00:00:00', 'Programación', 'prog01',15), (2,'2012-11-25 00:00:00', 'Matemática', 'mat01', 15), (3,'2012-11-25 00:00:00', 'Infraestructura', 'infra01', 10), (4,'2012-11-25 00:00:00', 'Electrónica', 'elect01', 10), (5,'2012-11-25 00:00:00', 'Física', 'fis01', 10), (6,'2012-11-25 00:00:00', 'Laboratorio', 'lab01', 10), (7,'2012-11-25 00:00:00', 'Química', 'quim01', 10), (8,'2012-11-25 00:00:00', 'Ciencias Biológicas', 'ciebio01',10);
 
-INSERT INTO `Materia` VALUES (1,'2012-11-25 15:37:58','Matematica','mat01',9);
+INSERT INTO `CarreraMateria` VALUES (1,1), (1,2), (1,3), (2,4), (2,5), (3,6), (3,7), (3,8);
 
-INSERT INTO `CarreraMateria` VALUES (5,1);
+INSERT INTO `Asignatura` VALUES (1,'2012-11-25 00:00:00','Principio Programación','pp1',5,FALSE,1), (2,'2012-11-25 00:00:00','Estructura de Datos y Algorítmos','eda1',5,FALSE,0), (3,'2012-11-25 00:00:00','Programación de Aplicaciones','pda1',5,FALSE,1), (4,'2012-11-25 00:00:00','Lógica 1','log1',5,FALSE,0), (5,'2012-11-25 00:00:00','Matemática Discreta','mdd1',5,FALSE,1), (6,'2012-11-25 00:00:00','Probabilidad y Estadística','pYd1',5,FALSE,0), (7,'2012-11-25 00:00:00','Sistemas Operativos','so1',5,TRUE,1), (8,'2012-11-25 00:00:00','Redes de Computadoras','redes1',5,TRUE,1), (9,'2012-11-25 00:00:00','Arquitectura de Software','arqSoft1',10,TRUE,0), (10,'2012-11-25 00:00:00','Electrónica 1','ee1',10,FALSE,1), (11,'2012-11-25 00:00:00','Física 2','fis2',10,FALSE,0), (12,'2012-11-25 00:00:00','Prevención de Riesgos','prev1',10,FALSE,1), (13,'2012-11-25 00:00:00','Química 2','quim2',10,FALSE,0), (14,'2012-11-25 00:00:00','Bioquímica Aplicada','bioqAp',10,FALSE,0);
 
-INSERT INTO `Asignatura` VALUES (1,'2012-11-25 15:37:57','Calculo 1','calc01',5,NULL,1),(2,'2012-11-25 15:37:57','Fisica 1','soyPrevia',8,NULL,0), (3,'2012-11-25 15:37:57','Algebra','al001',NULL,NULL,1), (4,'2012-11-25 15:37:57','Asign 2','Asignatura dos',NULL,NULL,1);
+INSERT INTO `AsigMaterias` VALUES (1,1), (1,2), (1,3), (2,4), (2,5), (2,6), (3,7), (3,8), (3,9), (4,10), (5,11), (6,12), (7,13), (8,14);
 
-INSERT INTO `AsigMaterias` VALUES (1,1),(1,2),(1,3),(1,4);
+INSERT INTO `Previas` VALUES (2,1), (3,2), (5,4), (6,5), (8,4), (8,2);
 
-INSERT INTO `Previas` VALUES (1,2);
-
-INSERT INTO `PeriodoInscripcion` VALUES (1,'2012-11-30 20:38:00','Periodo de Carrera',NULL,NULL,NULL,1,0),(2,'2012-11-30 20:38:00','Periodo de curso','2012-12-13 00:00:00','2012-12-30 00:00:00','2012-12-10 20:38:00',1,1),(3,'2012-11-30 20:38:00','Periodo de curso',NULL,NULL,NULL,1,2);
-
-INSERT INTO `Curso` VALUES (1,'2012-12-02 19:28:20','Calculo 1 vesp.','codigoCurso',NULL,'2012-12-02 19:28:20','2012-12-02 19:28:20','Los jueves de 9 a 10:30',NULL,'Par',1,1), (2,'2012-12-02 19:28:20','Prog. 3 1 vesp.','codigoCursoDos',NULL,'2012-12-02 19:28:20','2012-12-02 19:28:20','Los viernes de 9 a 10:30',NULL,'Impar',1,1);
-
-INSERT INTO `Examen` VALUES (1,'2012-12-04 22:52:21','codigo','2012-12-04 22:52:21',NULL,1);
-
---INSERT INTO `Inscripcion` VALUES (1,'2012-11-30 20:38:00',NULL,NULL,1,0,5,NULL,1,NULL,1),(2,'2012-11-30 20:38:00',NULL,NULL,1,1,NULL,1,1,NULL,2),(3,'2012-11-30 20:38:00',NULL,NULL,1,2,NULL,NULL,1,1,3);
-INSERT INTO `Inscripcion` VALUES (1,'2012-12-14 20:06:07',NULL,NULL,1,0,5,NULL,1,NULL,1),(2,'2012-12-14 20:06:07',NULL,NULL,1,1,NULL,1,1,NULL,2),(3,'2012-12-14 20:06:07',NULL,NULL,1,2,NULL,NULL,1,1,3);
-
---INSERT INTO `Evaluacion` VALUES (1,'2012-12-04 22:10:26',NULL,NULL,0,8,2,1,1);
-INSERT INTO `Evaluacion` VALUES (1,'2012-12-14 20:06:07',0,NULL,0,8,2,1,1,NULL),(2,'2012-12-14 20:06:07',0,NULL,0,8,2,NULL,1,1);
-
+INSERT INTO `Curso` VALUES (1,'2012-12-02 00:00:00','Principio de Programación','pp1',Descripcion,'2012-08-01 00:00:00','2012-03-01 00:00:00','Lunes y Martes de 9 a 11','102','Impar',0,1), (2,'2012-12-02 00:00:00','Estructura de Datos y Algorítmos','eda1',Descripcion,'2012-12-01 00:00:00', '2012-08-02 00:00:00', 'Miércoles y Viernes de 10.30 a 12','44','Par',0,2), (3,'2012-12-02 00:00:00','Programación de Aplicaciones','pda1',Descripcion,'2012-08-01 00:00:00','2012-03-01 00:00:00','Martes, Miércoes y Viernes de 11 a 12','107','Impar',0,3), (4,'2012-12-02 00:00:00','Lógica 1','log1',Descripcion,'2012-08-01 00:00:00','2012-03-01 00:00:00','Miércoles de 9 a 12','109','Impar',0,4), (5,'2012-12-02 00:00:00','Matemática Discreta','matD01',Descripcion, '2012-12-01 00:00:00', '2012-08-02 00:00:00','Sábados de 8 a 9.30','8','Par',0,5), (6,'2012-12-02 00:00:00','Probabilidad y Estadística','pYd1',Descripcion,'2012-08-01 00:00:00','2012-03-01 00:00:00','Lunes de 11 a 12 y Jueves de 10.30 a 12','Actos','Impar',0,6), (7,'2012-12-02 00:00:00','Sistemas Operativos 1','so1',Descripcion,'2012-08-01 00:00:00','2012-03-01 00:00:00','Jueves y Viernes de 10 a 11.30','107','Impar',0,7), (8,'2012-12-02 00:00:00','Redes 1','redes1',Descripcion, '2012-12-01 00:00:00', '2012-08-02 00:00:00','Lunes a Viernes de 9 a 11','107','Par',0,8), (9,'2012-12-02 00:00:00','Arquitectura de Software','arqSoft1',Descripcion,'2012-08-01 00:00:00','2012-03-01 00:00:00','Martes de 8.30 a 13','Actos','Impar',0,9);
